5b448e694869596e0d7a2021720c10562eb6dfd5
App Academy Student <student@appacademy.io>
2014-01-02 14:41:31 -0800

    can create a random asteroid

---
+++ asteroid.js
@@ -0,0 +1,20 @@
+(function (root) {
+	var Asteroids = root.Asteroids = (root.Asteroids || {});
+
+	var Asteroid = Asteroids.Asteroid = function (pos, vel, radius, color) {
+		Asteroids.MovingObject.apply(this, [pos, vel, radius, color]);
+	};
+
+	Asteroid.inherits(Asteroids.MovingObject);
+
+	Asteroid.COLOR = 'black';
+	Asteroid.RADIUS = 10;
+
+	Asteroid.randomAsteroid = function(dimX, dimY) {
+		var startingPos = [Math.floor(dimX * Math.random()), Math.floor(dimY * Math.random())];
+		// console.log("starting pos is " + startingPos)
+		var velocity = [10 * Math.random(), 10 * Math.random()];
+
+		return new Asteroid(startingPos, velocity, Asteroid.RADIUS, Asteroid.COLOR);
+	};
+})(this);
